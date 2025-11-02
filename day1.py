308ef8a33dd608c465a9176515f45d3e379bc32b
cattclawzz <anakinedwards9@gmail.com>
2025-11-02 20:27:31 +0000

    Might try and make this better but it works for now

--- day1.py
+++ day1.py
@@ -1,9 +1,13 @@
 def findFloor(floor, instructions):
-    for i in instructions:
-        if i == '(':
+    basement = 0
+    for char in range(len(instructions)):
+        if instructions[char] == '(':
             floor += 1
-        elif i == ')':
+        elif instructions[char] == ')':
             floor -= 1
-    return(floor)
+        if floor < 0 and basement == 0:
+            basement = char + 1
+
+    return(f"Part 1: {floor} \nPart 2: {basement}")
 
 print(findFloor(0, input()))
